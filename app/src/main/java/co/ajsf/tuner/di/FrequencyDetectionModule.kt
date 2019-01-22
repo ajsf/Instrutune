fb2b99f0e5a9e5ed7f551cd113ddb12a9db05426
Aaron Friedman <aaron.js.friedman@gmail.com>
2019-01-21 23:22:16 -1000

    Create separate Tarsos DI module, and update initialization settings.

--- app/src/main/java/co/ajsf/tuner/di/FrequencyDetectionModule.kt
+++ app/src/main/java/co/ajsf/tuner/di/FrequencyDetectionModule.kt
@@ -1,56 +1,20 @@
 package co.ajsf.tuner.di
 
-import be.tarsos.dsp.io.android.AudioDispatcherFactory
-import be.tarsos.dsp.pitch.PitchDetectionHandler
-import be.tarsos.dsp.pitch.PitchProcessor
 import co.ajsf.tuner.frequencydetection.DetectionEngine
 import co.ajsf.tuner.frequencydetection.FrequencyDetector
-import co.ajsf.tuner.mapper.TarsosResponseToModelMapper
-import co.ajsf.tuner.mapper.mapTarsosResponseToDetectionResult
-import co.ajsf.tuner.frequencydetection.tarsos.*
-import io.reactivex.schedulers.Schedulers
+import co.ajsf.tuner.frequencydetection.tarsos.TarsosDetectionEngine
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
 import org.kodein.di.generic.provider
 
-fun frequencyDetectionModule() = Kodein
-    .Module("frequencyDetectionModule") {
-        val sampleRate = 44100
-        val bufferSize = 2048
+fun frequencyDetectionModule() = Kodein.Module("frequencyDetectionModule") {
 
-        bind<FrequencyDetector>() with provider { FrequencyDetector(instance()) }
+    import(tarsosModule())
 
-        bind<DetectionEngine>() with provider {
-            TarsosDetectionEngine(instance(), instance())
-        }
+    bind<FrequencyDetector>() with provider { FrequencyDetector(instance()) }
 
-        bind<PitchProcessorBuilder>() with provider {
-            { pdh: PitchDetectionHandler ->
-                println("Detecter: building pitch processor")
-                PitchProcessor(
-                    PitchProcessor.PitchEstimationAlgorithm.MPM, sampleRate.toFloat(),
-                    bufferSize, pdh
-                )
-            }
-        }
-
-        bind<TarsosResponseToModelMapper>() with provider { ::mapTarsosResponseToDetectionResult }
-
-        bind<AudioDispatcherBuilder>() with provider {
-            {
-                println("Detecter: Building audio dispatcher")
-                AudioDispatcherFactory.fromDefaultMicrophone(
-                    sampleRate, bufferSize, 1024
-                )
-            }
-        }
-
-        bind<TarsosDispatcher>() with provider {
-            TarsosDispatcherImpl(instance(), instance())
-        }
-
-        bind<TarsosResponseStream>() with provider {
-            TarsosFlowable(instance(), Schedulers.io())
-        }
+    bind<DetectionEngine>() with provider {
+        TarsosDetectionEngine(instance(), instance())
     }
+}
---
+++ app/src/main/java/co/ajsf/tuner/di/TarsosModule.kt
@@ -0,0 +1,49 @@
+package co.ajsf.tuner.di
+
+import be.tarsos.dsp.io.android.AudioDispatcherFactory
+import be.tarsos.dsp.pitch.PitchDetectionHandler
+import be.tarsos.dsp.pitch.PitchProcessor
+import co.ajsf.tuner.frequencydetection.tarsos.*
+import co.ajsf.tuner.mapper.TarsosResponseToModelMapper
+import co.ajsf.tuner.mapper.mapTarsosResponseToDetectionResult
+import io.reactivex.schedulers.Schedulers
+import org.kodein.di.Kodein
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+
+fun tarsosModule() = Kodein
+    .Module("tarsosModule") {
+        val sampleRate = 44100
+        val bufferSize = 4096
+
+        bind<PitchProcessorBuilder>() with provider {
+            { pdh: PitchDetectionHandler ->
+                PitchProcessor(
+                    PitchProcessor.PitchEstimationAlgorithm.FFT_YIN,
+                    sampleRate.toFloat(),
+                    bufferSize,
+                    pdh
+                )
+            }
+        }
+
+        bind<TarsosResponseToModelMapper>() with provider { ::mapTarsosResponseToDetectionResult }
+
+        bind<AudioDispatcherBuilder>() with provider {
+            {
+                AudioDispatcherFactory
+                    .fromDefaultMicrophone(
+                        sampleRate, bufferSize, bufferSize / 2
+                    )
+            }
+        }
+
+        bind<TarsosDispatcher>() with provider {
+            TarsosDispatcherImpl(instance(), instance())
+        }
+
+        bind<TarsosResponseStream>() with provider {
+            TarsosFlowable(instance(), Schedulers.io())
+        }
+    }
