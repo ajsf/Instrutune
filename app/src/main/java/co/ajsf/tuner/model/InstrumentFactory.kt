3ceab1b866f4b3a67c01e26ee87b8e9d58c94495
Aaron Friedman <aaron.js.friedman@gmail.com>
2019-01-26 01:45:27 -1000

    Increase resolution of instrument tunings.

--- app/src/main/java/co/ajsf/tuner/model/InstrumentFactory.kt
+++ app/src/main/java/co/ajsf/tuner/model/InstrumentFactory.kt
@@ -3,18 +3,18 @@ package co.ajsf.tuner.model
 object InstrumentFactory {
     val GUITAR = Instrument(
         "Guitar", listOf(
-            InstrumentString(name = "E", freq = 82.41f),
+            InstrumentString(name = "E", freq = 82.407f),
             InstrumentString(name = "A", freq = 110f),
-            InstrumentString(name = "D", freq = 146.83f),
-            InstrumentString(name = "G", freq = 196f),
-            InstrumentString(name = "B", freq = 246.94f),
-            InstrumentString(name = "E", freq = 329.63f)
+            InstrumentString(name = "D", freq = 146.832f),
+            InstrumentString(name = "G", freq = 195.998f),
+            InstrumentString(name = "B", freq = 246.942f),
+            InstrumentString(name = "E", freq = 329.628f)
         )
     )
 
     val BASS = Instrument(
         "Bass", listOf(
-            InstrumentString(name = "E", freq = 41.204f),
+            InstrumentString(name = "E", freq = 41.203f),
             InstrumentString(name = "A", freq = 55f),
             InstrumentString(name = "D", freq = 73.416f),
             InstrumentString(name = "G", freq = 97.999f)
@@ -23,8 +23,8 @@ object InstrumentFactory {
 
     val BASS_5_LOW = Instrument(
         "Bass (5 String Low B)", listOf(
-            InstrumentString(name = "B", freq = 30.8f),
-            InstrumentString(name = "E", freq = 41.204f),
+            InstrumentString(name = "B", freq = 30.868f),
+            InstrumentString(name = "E", freq = 41.203f),
             InstrumentString(name = "A", freq = 55f),
             InstrumentString(name = "D", freq = 73.416f),
             InstrumentString(name = "G", freq = 97.999f)
@@ -33,40 +33,40 @@ object InstrumentFactory {
 
     val BASS_5_HI = Instrument(
         "Bass (5 String High C)", listOf(
-            InstrumentString(name = "E", freq = 41.204f),
+            InstrumentString(name = "E", freq = 41.203f),
             InstrumentString(name = "A", freq = 55f),
             InstrumentString(name = "D", freq = 73.416f),
             InstrumentString(name = "G", freq = 97.999f),
-            InstrumentString(name = "C", freq = 130.83f)
+            InstrumentString(name = "C", freq = 130.813f)
         )
     )
 
     val BASS_6 = Instrument(
         "Bass (6 String)", listOf(
-            InstrumentString(name = "B", freq = 30.8f),
-            InstrumentString(name = "E", freq = 41.204f),
+            InstrumentString(name = "B", freq = 30.868f),
+            InstrumentString(name = "E", freq = 41.203f),
             InstrumentString(name = "A", freq = 55f),
             InstrumentString(name = "D", freq = 73.416f),
             InstrumentString(name = "G", freq = 97.999f),
-            InstrumentString(name = "C", freq = 130.83f)
+            InstrumentString(name = "C", freq = 130.813f)
         )
     )
 
     val UKULELE = Instrument(
         "Ukulele (Standard)", listOf(
-            InstrumentString(name = "G", freq = 392f),
-            InstrumentString(name = "C", freq = 261.63f),
-            InstrumentString(name = "E", freq = 329.63f),
+            InstrumentString(name = "G", freq = 391.995f),
+            InstrumentString(name = "C", freq = 261.626f),
+            InstrumentString(name = "E", freq = 329.628f),
             InstrumentString(name = "A", freq = 440f)
         )
     )
 
     val BARITONE_UKULELE = Instrument(
         "Ukulele (Baritone)", listOf(
-            InstrumentString(name = "D", freq = 146.83f),
-            InstrumentString(name = "G", freq = 196f),
-            InstrumentString(name = "B", freq = 246.94f),
-            InstrumentString(name = "E", freq = 329.63f)
+            InstrumentString(name = "D", freq = 146.832f),
+            InstrumentString(name = "G", freq = 195.998f),
+            InstrumentString(name = "B", freq = 246.942f),
+            InstrumentString(name = "E", freq = 329.628f)
         )
     )
 
