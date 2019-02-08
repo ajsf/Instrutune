449e8776d98500caa13c45a3bc359247e443f200
Aaron Friedman <aaron.js.friedman@gmail.com>
2019-02-07 20:04:30 -1000

    Add OverridesRule for instrumentation DI, and RepeatRule for repeating tests to androidTest.

---
+++ app/src/androidTest/java/co/ajsf/tuner/rules/RepeatTest.kt
@@ -0,0 +1,5 @@
+package co.ajsf.tuner.rules
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.ANNOTATION_CLASS)
+annotation class RepeatTest(val value: Int = 1)
