1ae89dfddc265a782acaea5693b933a1d0c2da81
Aaron Friedman <aaron.js.friedman@gmail.com>
2019-03-30 23:10:06 -1000

    Refactor StringsView to extend from ConstraintLayout and TunerStringsView to extend from FrameLayout.

--- app/src/main/java/co/ajsf/tuner/features/common/view/StringsView.kt
+++ app/src/main/java/co/ajsf/tuner/features/common/view/StringsView.kt
@@ -2,20 +2,15 @@ package co.ajsf.tuner.features.common.view
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.Gravity
-import android.widget.LinearLayout
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import co.ajsf.tuner.R
 
 open class StringsView
 @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : LinearLayout(context, attrs, defStyleAttr) {
-
-    init {
-        orientation = LinearLayout.HORIZONTAL
-        gravity = Gravity.CENTER
-        clipChildren = false
-    }
+) : ConstraintLayout(context, attrs, defStyleAttr) {
 
     protected var stringViews = listOf<TunerStringView>()
 
@@ -27,12 +22,70 @@ open class StringsView
 
     open fun setStrings(numberedNames: List<String>) {
         removeAllViews()
-        stringViews = numberedNames.map { createStringView(it) }
+        stringViews = numberedNames.mapIndexed { index, s -> createStringView(s, index) }
+        val constraintSet = createConstraints()
+        constraintSet.applyTo(this)
     }
 
-    private fun createStringView(stringName: String): TunerStringView =
+    protected fun createConstraints(): ConstraintSet {
+        val constraintSet = ConstraintSet()
+        for (i in stringViews.indices) {
+            setConstraints(i, stringViews[i].id, constraintSet)
+            constraintSet.constrainWidth(stringViews[i].id, ConstraintSet.WRAP_CONTENT)
+        }
+        return constraintSet
+    }
+
+    protected fun setConstraints(index: Int, viewId: Int, constraintSet: ConstraintSet) {
+        if (index == 0) {
+            constraintSet.connect(
+                viewId,
+                ConstraintSet.LEFT,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.LEFT
+            )
+        } else {
+            constraintSet.connect(
+                viewId,
+                ConstraintSet.LEFT,
+                stringViews[index - 1].id,
+                ConstraintSet.RIGHT
+            )
+        }
+
+        if (index == stringViews.lastIndex) {
+            constraintSet.connect(
+                viewId,
+                ConstraintSet.RIGHT,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.RIGHT
+            )
+        } else {
+            constraintSet.connect(
+                viewId,
+                ConstraintSet.RIGHT,
+                stringViews[index + 1].id,
+                ConstraintSet.LEFT
+            )
+        }
+
+        constraintSet.connect(
+            viewId, ConstraintSet.TOP,
+            ConstraintSet.PARENT_ID, ConstraintSet.TOP
+        )
+        constraintSet.connect(
+            viewId, ConstraintSet.BOTTOM,
+            ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM
+        )
+    }
+
+    private fun createStringView(stringName: String, index: Int): TunerStringView =
         TunerStringView(context)
-            .apply { numberedName = stringName }
+            .apply {
+                id = View.generateViewId()
+                numberedName = stringName
+                transitionName = "$stringName$index"
+            }
             .also { addView(it) }
 
     fun setSelectedString(selectedStringName: String): Unit = stringViews
--- app/src/main/java/co/ajsf/tuner/features/common/view/TunerStringView.kt
+++ app/src/main/java/co/ajsf/tuner/features/common/view/TunerStringView.kt
@@ -3,24 +3,23 @@ package co.ajsf.tuner.features.common.view
 import android.animation.ObjectAnimator
 import android.content.Context
 import android.util.AttributeSet
-import android.view.Gravity
 import android.view.LayoutInflater
-import android.view.ViewGroup
 import android.view.animation.AnimationUtils
-import android.widget.LinearLayout
+import android.widget.FrameLayout
+import androidx.core.animation.doOnEnd
 import co.ajsf.tuner.R
 import kotlinx.android.synthetic.main.tuner_string.view.*
 
 class TunerStringView
 @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : LinearLayout(context, attrs, defStyleAttr) {
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private var currentAnimation: ObjectAnimator? = null
 
     init {
         LayoutInflater.from(context).inflate(R.layout.tuner_string, this)
-        layoutParams = LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT, 1f)
         string_name_outline_selected.imageAlpha = 0
-        gravity = Gravity.CENTER
     }
 
     private var stringIsSelected = false
@@ -31,32 +30,49 @@ class TunerStringView
             field = numberedName
         }
 
-    fun updateSelectedString(selectedString: String) {
+    fun updateSelectedString(selectedString: String, shakeString: Boolean = true) {
+        clearAnimations()
         if (selectedString == numberedName) {
             stringIsSelected = true
-            setSelected()
+            selectString(shakeString)
         } else if (stringIsSelected) {
             stringIsSelected = false
             unselect()
         }
     }
 
-    private fun setSelected() {
-        val stringShakeAnimation = AnimationUtils.loadAnimation(context, R.anim.string_shake)
-        string_image.startAnimation(stringShakeAnimation)
-        ObjectAnimator.ofInt(string_name_outline_selected, "imageAlpha", 255).apply {
+    private fun selectString(shakeString: Boolean = true) {
+        if (shakeString) {
+            val stringShakeAnimation = AnimationUtils.loadAnimation(context, R.anim.string_shake)
+            string_image.startAnimation(stringShakeAnimation)
+        }
+
+        currentAnimation = imageAlphaAnimation(0, 255).apply {
             duration = 200
             start()
         }
     }
 
     private fun unselect() {
-        string_image.clearAnimation()
         val shakeEndAnimation = AnimationUtils.loadAnimation(context, R.anim.string_shake_end)
         string_image.startAnimation(shakeEndAnimation)
-        ObjectAnimator.ofInt(string_name_outline_selected, "imageAlpha", 0).apply {
-            duration = 600
+
+        currentAnimation = imageAlphaAnimation(255, 0).apply {
+            duration = 1800
+            startDelay = 400
             start()
         }
+    }
+
+    private fun imageAlphaAnimation(fromValue: Int, toValue: Int): ObjectAnimator = ObjectAnimator
+        .ofInt(string_name_outline_selected, "imageAlpha", fromValue, toValue)
+        .apply {
+            doOnEnd { currentAnimation = null }
+        }
+
+    private fun clearAnimations() {
+        string_image.clearAnimation()
+        currentAnimation?.cancel()
+        currentAnimation = null
     }
 }
